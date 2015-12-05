30be9a2ee833d078d5e305f694fcce08f63df485
KIHARA Hideto <deton@m1.interq.or.jp>
2015-12-05 16:23:56 +0900

    Lync着信時にpresencestickのLEDを点灯するプログラム追加

---
+++ LyncRingNotify/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace LyncRingNotify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(12, 41);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.Size = new System.Drawing.Size(268, 188);
+            this.logTextBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 268);
+            this.Controls.Add(this.logTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox logTextBox;
+    }
+}
+
---
+++ LyncRingNotify/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using Microsoft.Lync.Model;
+using Microsoft.Lync.Model.Conversation;
+
+namespace LyncRingNotify
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _serial;
+        private LyncClient _lyncClient;
+        private delegate void DelegateWrite(string data);
+
+        public Form1()
+        {
+            InitializeComponent();
+            _serial = new SerialPort("COM10", 115200);
+            _serial.Open();
+
+            _lyncClient = LyncClient.GetClient();
+            _lyncClient.ConversationManager.ConversationAdded += ConversationManager_ConversationAdded;
+        }
+
+        void ConversationManager_ConversationAdded(object sender, Microsoft.Lync.Model.Conversation.ConversationManagerEventArgs e)
+        {
+            var conversation = e.Conversation;
+
+            // Test conversation state. If inactive, then the new conversation window was opened by the user, not a remote participant
+            if (conversation.State == ConversationState.Inactive)
+            {
+                return;
+            }
+
+            // Register for the ParticipantAdded event on the conversation
+            conversation.ParticipantAdded += Conversation_ParticipantAdded;
+
+            conversation.Modalities[ModalityTypes.InstantMessage].ModalityStateChanged += IMModalityStateChanged;
+            conversation.Modalities[ModalityTypes.AudioVideo].ModalityStateChanged += AVModalityStateChanged;
+
+            bool hasInstantMessaging = false;
+            if (ModalityIsNotified(conversation, ModalityTypes.InstantMessage))
+            {
+                hasInstantMessaging = true;
+                Invoke(new DelegateWrite(Write), new Object[] {"IM Notified"});
+                _serial.Write("y");
+            }
+            bool hasAudioVideo = false;
+            if (ModalityIsNotified(conversation, ModalityTypes.AudioVideo))
+            {
+                hasAudioVideo = true;
+                Invoke(new DelegateWrite(Write), new Object[] {"AV Notified"});
+                _serial.Write("r");
+            }
+
+            // Get the URI of the "Inviter" contact
+            var remoteParticipant = ((Contact)conversation.Properties[ConversationProperty.Inviter]).Uri;
+
+            Invoke(new DelegateWrite(Write), new Object[] {
+                    string.Format("Incoming Call\r\nCaller: {0}\r\nHas Instant Messaging: {1}\r\nHas Audio/Video: {2}",
+                        remoteParticipant,
+                        hasInstantMessaging,
+                        hasAudioVideo)
+                });
+        }
+
+        void Conversation_ParticipantAdded(object sender, ParticipantCollectionChangedEventArgs e)
+        {
+            var participant = e.Participant;
+
+            // We're not interested in notifying on our own IM's, so return
+            if (participant.IsSelf)
+            {
+                return;
+            }
+
+            // Get the InstantMessage modality, and register to the InstantMessageReceived event
+            var imModality = (InstantMessageModality)e.Participant.Modalities[ModalityTypes.InstantMessage];
+            imModality.InstantMessageReceived += ImModality_InstantMessageReceived;
+        }
+
+        void ImModality_InstantMessageReceived(object sender, MessageSentEventArgs e)
+        {
+            // Get the instant mesage modality that raised this event, and the contact that it belongs to
+            var imModality = (InstantMessageModality)sender;
+            var contact = imModality.Participant.Contact;
+
+            var instantMessageText = e.Text;
+
+            Invoke(new DelegateWrite(Write), new Object[] {
+                    string.Format("Caller: {0}\r\nText: {1}",
+                        contact.Uri,
+                        instantMessageText)
+                });
+        }
+
+        void IMModalityStateChanged(object sender, ModalityStateChangedEventArgs e)
+        {
+            switch (e.NewState)
+            {
+                case ModalityState.Connected:
+                    _serial.Write("o");
+                    Invoke(new DelegateWrite(Write), new Object[] {"IM Modality Connected"});
+                    break;
+                case ModalityState.Disconnected:
+                    Invoke(new DelegateWrite(Write), new Object[] {"IM Modality Disconnected"});
+                    _serial.Write("o");
+                    break;
+            }
+        }
+
+        void AVModalityStateChanged(object sender, ModalityStateChangedEventArgs e)
+        {
+            switch (e.NewState)
+            {
+                case ModalityState.Connected:
+                    _serial.Write("o");
+                    Invoke(new DelegateWrite(Write), new Object[] {"AV Modality Connected"});
+                    break;
+                case ModalityState.Disconnected:
+                    Invoke(new DelegateWrite(Write), new Object[] {"AV Modality Disconnected"});
+                    _serial.Write("o");
+                    break;
+            }
+        }
+
+        private bool ModalityIsNotified(Conversation conversation, ModalityTypes modalityType)
+        {
+            return conversation.Modalities.ContainsKey(modalityType) &&
+                   conversation.Modalities[modalityType].State == ModalityState.Notified;
+        }
+
+        /*!
+         * logTextBoxに受信内容を書き込みます。
+         */
+        private void Write(string data)
+        {
+            if (data != null)
+            {
+                logTextBox.AppendText(data + "\n");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_serial.IsOpen)
+            {
+                _serial.Close();
+            }
+        }
+    }
+}

41729b5096e9c27eb48b916b547dc373e6bccdec
KIHARA Hideto <deton@m1.interq.or.jp>
2015-12-05 15:06:02 +0900

    Use TimerThree library to blink motor

--- usbvibration/usbvibration.ino
+++ usbvibration/usbvibration.ino
@@ -1,59 +1,16 @@
-// https://github.com/thomasfredericks/Metro-Arduino-Wiring
-#include <Metro.h>
+// https://www.pjrc.com/teensy/td_libs_TimerOne.html
+#include <TimerThree.h>
 
-const int MOTORPIN = 2;
+const int MOTORPIN = 5;
 
-Metro blink = Metro(400);
-bool isVibrationOn = false;
-bool isMotorOn = false;
-uint32_t now;
-
-void onMotor(void)
-{
-    digitalWrite(MOTORPIN, HIGH);
-    isMotorOn = true;
-}
-
-void offMotor(void)
-{
-    digitalWrite(MOTORPIN, LOW);
-    isMotorOn = false;
-}
-
-void beginVibration(void)
-{
-    blink.reset();
-    isVibrationOn = true;
-}
-
-void endVibration(void)
-{
-    offMotor();
-    isVibrationOn = false;
-}
-
-void vibrationLoop(void)
-{
-    if (!isVibrationOn) {
-        return;
-    }
-    if (blink.check()) {
-        if (isMotorOn) {
-            offMotor();
-        } else {
-            onMotor();
-        }
-    }
-}
-
-void parseMessage(char letter)
+void parseMessage(int letter)
 {
     switch (letter) {
     case 'v':
-        beginVibration();
+        Timer3.pwm(MOTORPIN, 512);
         break;
     case 'V':
-        endVibration();
+        Timer3.pwm(MOTORPIN, 0);
         break;
     default:
         break;
@@ -64,26 +21,11 @@ void setup()
 {
     Serial.begin(115200);
     Mouse.begin();
-    pinMode(MOTORPIN, OUTPUT);
-}
-
-static void mouseLoop()
-{
-    // move mouse to avoid screen saver
-    static uint32_t mprevms = 0;
-    const uint32_t PCLOCKMS = 540000; // 9 [min]
-    if (now - mprevms > PCLOCKMS) {
-        mprevms = now;
-        Mouse.move(1, 0, 0);
-        //Serial.write("M");
-    }
+    Timer3.initialize(400000);
 }
 
 void loop()
 {
-    now = millis();
-    vibrationLoop();
-    mouseLoop();
     while (Serial.available()) {
         char letter = Serial.read();
         parseMessage(letter);
